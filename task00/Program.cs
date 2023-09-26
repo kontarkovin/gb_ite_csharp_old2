0710829cfc0145f3844b0fee816c933926aa4314
Igor Shtengelov <kontarkovi@yandex.com>
2023-09-26 19:59:29 +0500

    Added Program.cs file

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿/* 
+Напишите программу, которая 
+1. на вход принимает число и 
+2. выдаёт его квадрат (число, умноженное на само себя)
+3. выводит результат
+
+Например:
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"{number} -> {square}");
