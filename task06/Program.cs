b81dafe31abd28c6968ebce2e1032e938f45a92f
Igor Shtengelov <kontarkovi@yandex.com>
2023-09-26 20:04:24 +0500

    Added task06. Homework

---
+++ task06/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая 
+1. на вход принимает число и 
+2. выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Наример:
+4 -> ДА
+-3 -> НЕТ
+7 -> НЕТ
+*/
+
+Console.WriteLine();
+Console.WriteLine("*** ПРОВЕРЯЕТ, ЯВЛЯЕТСЯ ЛИ ЧИСЛО ЧЁТНЫМ ***");
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{number} -> ");
+if (number % 2 == 0)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("НЕТ");
+}
