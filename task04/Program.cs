00d91e5042935e75ceacf36bcc705ef65bb5f266
Igor Shtengelov <kontarkovi@yandex.com>
2023-09-26 20:04:09 +0500

    Added task04. Homework

---
+++ task04/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите программу, которая 
+1. принимает на вход три числа и 
+2. выдаёт максимальное из этих чисел
+3. выводит результат
+
+Например:
+2, 3, 7 -> 7
+44, 5, 78  -> 78
+22, 3, 9, -> 22
+*/
+
+Console.WriteLine("ВВЕДИТЕ ТРИ ЧИСЛА!");
+Console.WriteLine("Число 1:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число 2:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число 3:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.WriteLine($"{num1}, {num2}, {num3} -> max = {max}");
