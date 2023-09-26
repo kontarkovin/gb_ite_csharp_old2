605ff6430df3606783251bc000e1c665f9f7b5aa
Igor Shtengelov <kontarkovi@yandex.com>
2023-09-26 20:00:25 +0500

    Added task02. Homework

---
+++ task02/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая 
+1. на вход принимает два числа и 
+2. выдаёт, какое число большее, а какое меньшее
+3. выводит результат
+
+Например:
+a = 5, b = 7 -> max = 7
+a = 2, b = 10 -> max = 10
+a = -9, b = -3 -> max = -3
+*/
+
+Console.WriteLine("Введите число 'a':");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 'b':");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"a = {number1}, b = {number2} -> max = ");
+if (number1 > number2)
+{
+   Console.WriteLine (number1);
+}
+else if (number1 < number2)
+{
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.WriteLine($"a = b = {number1}");
+}
