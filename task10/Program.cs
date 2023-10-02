361bf5b9dcf6288eae7973e39b15b5730a0dcf65
Igor Shtengelov <kontarkovi@yandex.com>
2023-10-02 11:08:41 +0500

    Added task10. Homework

---
+++ task10/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая 
+1. принимает на вход трёхзначное число и 
+2. на выходе показывает вторую цифру этого числа.
+
+Например:
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+
+int GetIntNumber(string req)
+{
+    Console.WriteLine(req);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+Console.WriteLine();
+Console.WriteLine("*** ПОКАЗЫВАЕТ ВТОРУЮ ЦИФРУ ЧИСЛА ***");
+int num = GetIntNumber("Введите трёхзначное целое число:");
+string secondDigit = (num > 99) & (num < 1000) ? Convert.ToString(num % 100 / 10) : "не является трёхзначным";
+Console.WriteLine($"{num} -> {secondDigit}");
