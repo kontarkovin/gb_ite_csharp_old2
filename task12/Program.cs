6c46a55924ad9baf3ef602c5db935d47b63a6e96
Igor Shtengelov <kontarkovi@yandex.com>
2023-10-02 11:07:53 +0500

    Added task12

---
+++ task12/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, котороя 
+1. будет принимать на вход два числа и 
+2. выводить, является ли первое число кратным второму.
+3. Если первое число не кратно второму, то программа выводит остаток от деления.
+
+34, 5 -> не кратно, остаток 4
+16, 4 -> кратно
+*/
+
+int GetIntNumber(string req)
+{
+    Console.WriteLine(req);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine();
+Console.WriteLine("*** ПРОВЕРЯЕТ КРАТНОСТЬ ПЕРВОГО ЧИСЛА ВТОРОМУ ***");
+int numberOne = GetIntNumber("Введите число 1:");
+int numberTwo = GetIntNumber("Введите число 2:");
+int remainder = numberOne % numberTwo;
+string message = Convert.ToBoolean(remainder) ? $"не кратно, остаток {remainder}" : "кратно";
+Console.WriteLine($"{numberOne}, {numberTwo} -> {message}");
